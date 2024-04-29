fe8aa979b09505aa346b75c498aec597f392424b
Asad khatri <92842688+AsadKhatri1@users.noreply.github.com>
2024-04-29 17:32:09 +0500

    trying deploy

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ config({ path: "./config/config.env" });
 //----------------------------- middlewears ------------------------------
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
