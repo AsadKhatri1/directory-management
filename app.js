b553a6eb04210355c3847f3c9ba7af0e7ef1c465
Asad khatri <92842688+AsadKhatri1@users.noreply.github.com>
2024-05-09 14:14:41 +0500

    9-5 cors

--- app.js
+++ app.js
@@ -19,19 +19,13 @@ config({ path: "./config/config.env" });
 //   })
 // );
 // Allow requests from specific origins
-const allowedOrigins = ["https://directory--sigma.vercel.app"];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  })
-);
 
+app.use({
+  origin: "https://directory--sigma.vercel.app",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+});
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
