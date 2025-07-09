2915334c44470c5aef12d4557db4edde3c7d59b8
AsadKhatri1 <hafizasad398@gmail.com>
2025-07-09 16:30:20 +0500

    tanent/owner fix

--- controllers/residentController.js
+++ controllers/residentController.js
@@ -118,17 +118,11 @@ export const allResidents = async (req, res) => {
 export const getResidentByHouse = async (req, res) => {
   try {
     const { houseNo, type } = req.params;
-    let resident_type;
-    if (type == "owner") {
-      resident_type = "tenant";
-    } else {
-      resident_type = "owner";
-    }
 
     const residents = await residentModel
       .find({
         HouseNumber: houseNo,
-        residentType: resident_type,
+        residentType: { $ne: type },
       })
       .sort({ createdAt: -1 });
 
