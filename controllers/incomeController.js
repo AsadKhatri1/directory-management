4106fe4b4b6eeb329d406e698d5cb06044fc18b3
RashidKhalique <rashidkhanjamali26@gmail.com>
2025-07-12 11:24:58 +0500

    Debug simpel22

--- controllers/incomeController.js
+++ controllers/incomeController.js
@@ -111,5 +111,9 @@ export const deleteIncome = async(req,res) =>{
   if(!id){
     return res.status(400).json({success : false , message : "Income ID is required"})
   } 
-  return res.json(200).json({ success : true, message : "Delete"})
+  const income = await incomeModel.findById(id);
+  if(!income){
+    return res.status(404).json({success : false , message : "Income not found"})
+  }
+  return res.status(200).json({ success : true, message : "Delete" , income})
 }
